1f03812ce9a0eaf7c034ec70637280ea03c68342
daichi-matsumoto <caios.system@gmail.com>
2023-06-04 19:24:05 +0900

    create BottomSheet animation

--- app/src/main/java/caios/android/kanade/ui/KanadeApp.kt
+++ app/src/main/java/caios/android/kanade/ui/KanadeApp.kt
@@ -8,20 +8,19 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
@@ -33,6 +32,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
@@ -73,8 +73,11 @@ fun KanadeApp(
             gesturesEnabled = true,
             scrimColor = Color.Transparent,
         ) {
-            var libraryTopBarHeight by remember { mutableStateOf(0.dp) }
-            val snackbarHostState = remember { SnackbarHostState() }
+            var topBarHeight by remember { mutableStateOf(0f) }
+            var bottomBarHeight by remember { mutableStateOf(0f) }
+            var bottomSheetHeight by remember { mutableStateOf(0f) }
+
+            var bottomSheetOffsetRate by remember { mutableStateOf(-1f) }
 
             val scope = rememberCoroutineScope()
             val scaffoldState = rememberBottomSheetScaffoldState()
@@ -82,17 +85,28 @@ fun KanadeApp(
             val topAppBarState = rememberLibraryTopBarScrollState()
             val scrollBehavior = LibraryTopBarScrollBehavior(
                 state = topAppBarState,
-                topBarHeight = with(density) { libraryTopBarHeight.toPx() },
+                topBarHeight = topBarHeight
             )
+
+            bottomSheetOffsetRate = try {
+                val offset = scaffoldState.bottomSheetState.requireOffset()
+                val defaultHeight = bottomSheetHeight - bottomBarHeight - with(density) { 64.dp.toPx() }
+
+                (offset / defaultHeight).coerceIn(0f, 1f)
+            } catch (e: Throwable) {
+                1f
+            }
 
             Scaffold(
                 modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
                 containerColor = Color.Transparent,
                 contentColor = MaterialTheme.colorScheme.onBackground,
                 contentWindowInsets = WindowInsets(0, 0, 0, 0),
-                snackbarHost = { SnackbarHost(snackbarHostState) },
                 bottomBar = {
                     KanadeBottomBar(
+                        modifier = Modifier
+                            .onGloballyPositioned { bottomBarHeight = it.size.height.toFloat() }
+                            .offset(y = with(density) { bottomBarHeight.toDp() } * (1f - bottomSheetOffsetRate)),
                         destination = appState.libraryDestinations,
                         onNavigateToDestination = appState::navigateToLibrary,
                         currentDestination = appState.currentDestination,
@@ -105,6 +119,7 @@ fun KanadeApp(
                 BottomSheetScaffold(
                     modifier = Modifier
                         .fillMaxSize()
+                        .onGloballyPositioned { bottomSheetHeight = it.size.height.toFloat() }
                         .padding(padding)
                         .consumeWindowInsets(padding)
                         .windowInsetsPadding(
@@ -113,26 +128,24 @@ fun KanadeApp(
                             ),
                         ),
                     scaffoldState = scaffoldState,
+                    sheetShape = RectangleShape,
+                    sheetDragHandle = {},
                     sheetContent = {
                         BottomController(
-                            bottomSheetState = scaffoldState.bottomSheetState,
+                            state = scaffoldState.bottomSheetState,
+                            offsetRate = bottomSheetOffsetRate,
                             onClickController = {},
                         )
                     },
-                    contentColor = Color.Transparent,
-                    backgroundColor = Color.Transparent,
-                    snackbarHost = { SnackbarHost(snackbarHostState) },
-                    sheetPeekHeight = 64.dp,
+                    containerColor = Color.Transparent,
+                    contentColor = MaterialTheme.colorScheme.onBackground,
+                    sheetPeekHeight = 64.dp + with(density) { bottomBarHeight.toDp() },
                 ) {
                     Box {
                         LibraryTopBar(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .onGloballyPositioned {
-                                    libraryTopBarHeight = with(density) {
-                                        it.size.height.toDp()
-                                    }
-                                }
+                                .onGloballyPositioned { topBarHeight = it.size.height.toFloat() }
                                 .zIndex(1f),
                             onClickMenu = {
                                 scope.launch {
@@ -145,7 +158,7 @@ fun KanadeApp(
 
                         KanadeNavHost(
                             appState = appState,
-                            libraryTopBarHeight = libraryTopBarHeight,
+                            libraryTopBarHeight = with(density) { topBarHeight.toDp() },
                         )
                     }
                 }
--- core/ui/src/main/java/caios/android/kanade/core/ui/controller/BottomController.kt
+++ core/ui/src/main/java/caios/android/kanade/core/ui/controller/BottomController.kt
@@ -2,41 +2,26 @@ package caios.android.kanade.core.ui.controller
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.BottomSheetState
-import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.constraintlayout.compose.ConstraintLayout
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun BottomController(
-    bottomSheetState: BottomSheetState,
+    state: SheetState,
+    offsetRate: Float,
     onClickController: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var oldProgress by remember { mutableStateOf(bottomSheetState.currentValue) }
-    val dragProgress = when (bottomSheetState.currentValue) {
-        BottomSheetValue.Collapsed -> {
-            if (oldProgress != BottomSheetValue.Collapsed) 0f else bottomSheetState.progress
-        }
-        BottomSheetValue.Expanded -> {
-            if (oldProgress != BottomSheetValue.Expanded) 1f else 1f - bottomSheetState.progress
-        }
-    }
-
-    oldProgress = bottomSheetState.currentValue
-
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Red.copy(green = dragProgress)),
+            .background(Color.Red.copy(green = offsetRate)),
     ) {
     }
 }
