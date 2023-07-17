8c1d006c01318d930ef8625ee54002b8ae34bac7
daichi-matsumoto <caios.system@gmail.com>
2023-07-17 21:09:03 +0900

    add setting module

--- feature/information/src/main/java/caios/android/kanade/feature/information/versions/VersionHistoryDialog.kt
+++ feature/information/src/main/java/caios/android/kanade/feature/information/versions/VersionHistoryDialog.kt
@@ -5,22 +5,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import caios.android.kanade.core.design.R
-import caios.android.kanade.core.design.theme.center
 import caios.android.kanade.core.design.theme.end
 import caios.android.kanade.core.design.theme.start
 import caios.android.kanade.core.model.UserData
@@ -30,29 +34,35 @@ import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 import java.time.format.DateTimeFormatter
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun VersionHistoryDialog(
     versionHistory: ImmutableList<Version>,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier
-            .statusBarsPadding()
-            .navigationBarsPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 24.dp, vertical = 16.dp),
-            text = stringResource(R.string.about_support_version_history),
-            style = MaterialTheme.typography.bodyLarge.center(),
-            color = MaterialTheme.colorScheme.onSurface,
-        )
+    val state = rememberTopAppBarState()
+    val behavior = TopAppBarDefaults.pinnedScrollBehavior(state)
 
-        Divider()
-
-        LazyColumn(Modifier.weight(1f)) {
+    Scaffold(
+        modifier = modifier.nestedScroll(behavior.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                title = {
+                    Text(
+                        text = stringResource(R.string.about_support_version_history),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+                },
+                scrollBehavior = behavior,
+            )
+        },
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = paddingValues,
+        ) {
             items(
                 items = versionHistory,
                 key = { item -> item.code },
