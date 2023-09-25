0c89adc9b927361a85f8a619d6198bab61e67563
daichi-matsumoto <caios.system@gmail.com>
2023-09-25 23:48:22 +0900

    add glance library

---
+++ feature/widget/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("kanade.library")
+    id("kanade.library.compose")
+    id("kanade.hilt")
+    id("kanade.detekt")
+}
+
+android {
+    namespace = "caios.android.kanade.feature.widget"
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:common"))
+    implementation(project(":core:repository"))
+    implementation(project(":core:datastore"))
+    implementation(project(":core:design"))
+    implementation(project(":core:music"))
+    implementation(project(":core:ui"))
+    implementation(project(":core:billing"))
+
+    implementation(libs.bundles.ui.implementation)
+    implementation(libs.bundles.glance)
+    kapt(libs.bundles.ui.kapt)
+}
